0dd01a19f0e084eee2b0b908da742e907170878e
Rusfighter <therusfighter@gmail.com>
2015-07-27 20:48:32 +0200

    Init

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	public Transform player;
+
+	private NavMeshAgent nav;
+	private Animator charAnimator;
+
+	// Use this for initialization
+	void Start () {
+		charAnimator = transform.GetChild (0).GetComponent<Animator> ();
+		nav = GetComponent <NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (player.position);
+		if (nav.velocity.magnitude > 0.5f) {
+			charAnimator.SetFloat ("Forward", 0.6f);
+		}else
+			charAnimator.SetFloat ("Forward", 0);
+	}
+}
---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (NavMeshAgent))]
+[RequireComponent (typeof (CapsuleCollider))]
+
+public class PlayerCtrl : MonoBehaviour {
+	private Animator charAnimator;
+	private Animator weaponsAnimator;
+	private NavMeshAgent nav;   
+
+	private Transform focus;
+	// Use this for initialization
+	void Start () {
+		charAnimator = transform.GetChild (0).GetComponent<Animator> ();
+		//weaponsAnimator = transform.GetChild (1).GetComponent<Animator> ();
+
+		nav = GetComponent <NavMeshAgent> ();
+
+		//charAnimator.SetFloat ("Speed_f", 1);
+		charAnimator.SetInteger ("WeaponType_int", 2);
+		//charAnimator.SetBool ("Shoot_b", true);
+
+		//weaponsAnimator.SetInteger ("WeaponType_int", 2);
+		//weaponsAnimator.SetBool ("Shoot_b", true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			ClickAction(Input.mousePosition);
+
+		} else if (Input.touchCount > 0) {
+			ClickAction(Input.GetTouch (0).position);
+		}
+
+		if (focus != null) {
+			Vector3 delta = -transform.position + focus.position;
+			transform.rotation = Quaternion.Lerp (transform.rotation, Quaternion.LookRotation (delta), 20 * Time.deltaTime);
+		} else
+			nav.updateRotation = true;
+
+
+		//
+		//normalize vectors
+		Vector3 lookTo = transform.forward.normalized;
+		lookTo.y = 0;
+		Debug.Log (lookTo);
+		Vector3 moveDirection = nav.velocity.normalized;
+		moveDirection.y = 0;
+
+		float angle = Vector3.Angle (moveDirection, lookTo) * Mathf.Deg2Rad;
+		charAnimator.SetFloat ("Forward", Mathf.Cos (angle) * nav.velocity.magnitude / nav.speed);
+
+
+		lookTo = Vector3.Cross (lookTo, Vector3.up);
+		angle = Vector3.Angle (moveDirection, lookTo) * Mathf.Deg2Rad;
+		charAnimator.SetFloat ("Turn", Mathf.Cos (angle) * nav.velocity.magnitude / nav.speed);
+
+	}
+
+	void ClickAction(Vector3 position){
+		Ray ray = Camera.main.ScreenPointToRay (position);
+		RaycastHit hit;
+		if (Physics.Raycast (ray, out hit, 100)) {
+			if (hit.collider.tag == "Enemy"){
+				Debug.Log ("Enemy");
+				focus = hit.collider.transform;
+				nav.ResetPath();
+				nav.updateRotation = false;
+			}else{
+				nav.SetDestination (hit.point);
+				//focus = null;
+			}
+		}
+	}
+}
